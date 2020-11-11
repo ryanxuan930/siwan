4dc8aa1192830ca92b9b01a3a18bd55140b6b693
ryanxuan930 <anakinchung930@gmail.com>
2020-11-11 17:08:01 +0800

    1111 5.07

--- Main/index.php
+++ Main/index.php
@@ -2,9 +2,19 @@
 <html>
 <head>
 <meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="css/main.css">
+<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
 <title>Siwan System</title>
 </head>
-
 <body>
+	<div class="bg-img">
+  		<form action="/action_page.php" class="container">
+    		<h1>Let's GO!</h1>
+			<label for="id"><b>Studet ID</b></label>
+    		<input type="text" placeholder="Enter Your Student ID" name="id" required>
+			<button type="submit" class="btn">START!</button>
+  		</form>
+	</div>
 </body>
 </html>

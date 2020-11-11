fc7340b7c80ab56c925a8baa8cb2534b747fdd93
ryanxuan930 <anakinchung930@gmail.com>
2020-11-11 23:35:53 +0800

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -15,7 +15,7 @@
     			<h1>SEIAWAN SYSTEM</h1>
 				<label for="id" style="color: lightgray">請輸入學號</label>
     			<input type="text" placeholder="Enter Your Student ID" name="id" required>
-				<label for="password" style="color: lightgray">請輸入學號</label>
+				<label for="password" style="color: lightgray">請輸入密碼</label>
     			<input type="password" placeholder="Enter Your Password" name="password" required>
 				<button type="submit" class="login_btn">登入</button>
   			</form>

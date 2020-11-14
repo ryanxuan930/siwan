295e1880a83039fac11b0f7d18dbf98b03762eef
ryanxuan930 <anakinchung930@gmail.com>
2020-11-15 01:01:58 +0800

    Update login.php

--- admin/module/auth/login.php
+++ admin/module/auth/login.php
@@ -1,11 +1,11 @@
 <?php
 include('../../../dbclass.php');
 include('../../../database.php');
-$id = $_POST['id'];
+$account = $_POST['id'];
 $password = $_POST['password'];
 $conn->select_db('seiawan');
-$stmt = $conn->prepare("SELECT * FROM admin WHERE id=? AND password=?");
-$stmt->bind_param("ss", $id, $password);
+$stmt = $conn->prepare("SELECT * FROM admin WHERE account=? AND password=?");
+$stmt->bind_param("ss", $account, $password);
 $stmt->execute();
 $result = $stmt->get_result();
 if($result->num_rows){

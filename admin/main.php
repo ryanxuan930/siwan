b73a2c11bc1420d627501ec4954d57928e426460
ryanxuan930 <anakinchung930@gmail.com>
2020-11-15 01:13:06 +0800

    Update main.php

--- admin/main.php
+++ admin/main.php
@@ -8,7 +8,25 @@
 <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
 <title>Seiawan</title>
 </head>
-
 <body>
+	<div class="topnav" id="topnav">
+  		<a href="#home" class="active">Home</a>
+  		<a href="#news">News</a>
+  		<a href="#contact">Contact</a>
+  		<a href="#about">About</a>
+  		<a href="javascript:void(0);" class="icon" onclick="topNav()">
+    		<i class="fa fa-bars"></i>
+  		</a>
+	</div>
+<script>
+function topNav(){
+	var x = document.getElementById("topnav");
+  	if(x.className === "topnav"){
+    	x.className += " responsive";
+  	}else{
+    	x.className = "topnav";
+  	}
+}
+</script>
 </body>
 </html>

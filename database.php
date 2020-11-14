1cc48bb735a1c47a806567aaaa1b6d728782769e
ryanxuan930 <anakinchung930@gmail.com>
2020-11-15 00:57:13 +0800

    Update database.php

--- database.php
+++ database.php
@@ -1,10 +1,12 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>Untitled Document</title>
-</head>
-
-<body>
-</body>
-</html>
+<?php
+$dbserver="127.0.0.1";
+$dbusername="seiawan";
+$dbpassword="Siwan075252000";
+$conn = new mysqli($dbserver, $dbusername, $dbpassword);
+//Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+$conn->query("set names utf8");
+date_default_timezone_set('Asia/Taipei');
+?>

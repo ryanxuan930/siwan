450b170d4bce9a7640d022639f3f69854a7bbd53
ryanxuan930 <anakinchung930@gmail.com>
2020-11-16 01:17:03 +0800

    1116 1.17

--- admin/module/home/index.php
+++ admin/module/home/index.php
@@ -1,3 +1,9 @@
 <?php
-echo '<div style="width: 100%; background-color:red; height: 100%;">TEST</div>';
+echo '<div class="console box">';
+echo '<h2>統計資訊</h2>';
+echo '<hr>';
+echo '<div>現在時間：2020-12-21 12:00</div>';
+echo '<div>目前參加人數：20 人</div>';
+echo '<div>累計參加人數：120 人</div>';
+echo '</div>';
 ?>

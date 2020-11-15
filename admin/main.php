43d6390399998eb75324386fd7cc4cfad06fc236
ryanxuan930 <anakinchung930@gmail.com>
2020-11-15 23:54:50 +0800

    Update main.php

--- admin/main.php
+++ admin/main.php
@@ -40,7 +40,7 @@ function homepage(){
 	currentPageNav(0);
 	topNav();
 	$.post("home/index.php",{query: 1},function(data){
-		$.("#container").html;
+		$.("#container").html();
 	});
 }
 function pointpage(){

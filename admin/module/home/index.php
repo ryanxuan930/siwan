4b3a248b7f00fbfdff4717988c8ba24971f13d0a
ryanxuan930 <anakinchung930@gmail.com>
2020-11-16 11:11:29 +0800

    Update index.php

--- admin/module/home/index.php
+++ admin/module/home/index.php
@@ -2,8 +2,20 @@
 echo '<div class="console box">';
 echo '<h2>統計資訊</h2>';
 echo '<hr>';
-echo '<div>現在時間：2020-12-21 12:00</div>';
+echo '<div>更新時間：2020-12-21 12:00:00</div>';
 echo '<div>目前參加人數：20 人</div>';
 echo '<div>累計參加人數：120 人</div>';
+echo '</div>';
+echo '<div class="console box">';
+echo '<h2>人流動態</h2>';
+echo '<hr>';
+echo '<div>更新時間：2020-12-21 12:00:00</div>';
+echo '<div>靜態展區：</div>';
+echo '<div>舒壓植物園：</div>';
+echo '<div>打開新世界：</div>';
+echo '<div>趨勢聯想：</div>';
+echo '<div>溝通媒介：</div>';
+echo '<div>摩斯密碼：</div>';
+echo '<div>聽力輔助：</div>';
 echo '</div>';
 ?>

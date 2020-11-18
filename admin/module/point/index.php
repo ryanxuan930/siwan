b6344135453da01ea42242096b616466f9e44831
ryanxuan930 <anakinchung930@gmail.com>
2020-11-19 01:38:33 +0800

    Update index.php

--- admin/module/point/index.php
+++ admin/module/point/index.php
@@ -1,10 +1,8 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>Untitled Document</title>
-</head>
-
-<body>
-</body>
-</html>
+<?php
+echo '<div class="console box">';
+echo '<h2>通關認證</h2>';
+echo '<hr>';
+echo '<label>通關者學號：</label>';
+echo '<input type="text" name="student" placeholder="請輸入通關者學號" />';
+echo '</div>';
+?>

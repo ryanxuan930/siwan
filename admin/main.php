3f9a68721bb07e8425400578bd3eb36b846d6a38
ryanxuan930 <anakinchung930@gmail.com>
2020-11-19 02:23:51 +0800

    Update main.php

--- admin/main.php
+++ admin/main.php
@@ -104,22 +104,6 @@ function configpage(){
 function logout(){
 	location.href = "module/auth/logout.php";
 }
-$(document).ready(function(){
-	$("#section_select").on("change",function(){
-		alert("change");
-		$.ajax({
-			type: "POST",
-			url: "module/point/section.php" ,
-			data: $("#section").serialize(),
-			success: function(result){
-				alert("ok");
-			},
-			error: function(){
-				alert("error");
-			}
-		});
-	});
-});	
 //ini
 homepage();
 topNav();

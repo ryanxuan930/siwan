aafc82097724d67f612b3a9cbbfac145d9eeb5b9
ryanxuan930 <anakinchung930@gmail.com>
2020-11-19 02:30:55 +0800

    Update section.php

--- admin/module/point/section.php
+++ admin/module/point/section.php
@@ -1,4 +1,23 @@
 <?php
 $section = $_POST['section'];
-echo $section;
+switch($section){
+	case 1:
+		echo '舒壓植物園';
+		break;
+	case 2:
+		echo '打開新世界';
+		break;
+	case 3:
+		echo '趨勢聯想';
+		break;
+	case 4:
+		echo '溝通媒介';
+		break;
+	case 5:
+		echo '摩斯密碼';
+		break;
+	case 6:
+		echo '聽力輔助';
+		break;
+}
 ?>

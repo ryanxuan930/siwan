a4c00cec0c60a95d99cffc8ea4cab82dcd663f1d
ryanxuan930 <anakinchung930@gmail.com>
2020-11-19 02:03:08 +0800

    1119 2.03

--- admin/js/point.js
+++ admin/js/point.js
@@ -1,3 +1,3 @@
-$.post("../module/point/section.php", {section_select.serialize}, function(){
-	alert("已設定關卡");
+$.post("../module/point/section.php", {section_select.serialize}, function(data){
+	alert(data);
 });
--- admin/module/point/section.php
+++ admin/module/point/section.php
@@ -1,10 +1,3 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>Untitled Document</title>
-</head>
-
-<body>
-</body>
-</html>
+<?php
+echo 'ok';
+?>

89964dd0637f7685e6f482e1c582f9346d6779b3
ryanxuan930 <anakinchung930@gmail.com>
2020-11-19 01:46:36 +0800

    Update index.php

--- admin/module/point/index.php
+++ admin/module/point/index.php
@@ -1,8 +1,12 @@
 <?php
 echo '<div class="console box">';
+echo '<h2>關卡資訊</h2>';
+echo '<hr>';
+echo '</div>';
+echo '<div class="console box">';
 echo '<h2>通關認證</h2>';
 echo '<hr>';
 echo '<input type="text" style="width: 100%;padding: 12px 20px;margin: 8px 0;box-sizing: border-box;" name="student" placeholder="請輸入通關者學號" />';
-echo '<button style="color:white;background-color: #E65100;width: 100%;padding: 12px 20px;margin: 8px 0;box-sizing: border-box;border: none;border-radius: 4px;cursor: pointer;">通關</button>';
+echo '<button style="color:white;background-color: #E65100;width: 100%;padding: 12px 20px;margin: 8px 0;box-sizing: border-box;border: none;border-radius: 4px;cursor: pointer;">完成本關卡</button>';
 echo '</div>';
 ?>

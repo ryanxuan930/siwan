39f9f95d8ae4d73429deb656a14f3213717ae0d2
ryanxuan930 <anakinchung930@gmail.com>
2020-11-19 22:23:29 +0800

    Update index.php

--- admin/module/point/index.php
+++ admin/module/point/index.php
@@ -35,6 +35,14 @@ function query(input){
 		alert(e);
 	});
 }
+$(document).ready(function(){
+	$(#pass_button).on("click",function(){
+		var input = $(#pass).val();
+		$.post("module/point/pass.php",{id: input, host: "'.$_SESSION['account'].'"},function(e){
+			alert(e);
+		});
+	});
+});
 </script>
 ';
 echo '<div class="console box">';
@@ -55,13 +63,11 @@ echo '</div>';
 echo '<div class="console box">';
 echo '<h2>通關認證</h2>';
 echo '<hr>';
-echo '<form id="pass">';
-echo '<input type="text" style="width: 100%;padding: 12px 20px;margin: 8px 0;box-sizing: border-box;" name="student" placeholder="請輸入通關者學號" />';
-echo '<button id="pass_button" style="color:white;background-color: #E65100;width: 100%;padding: 12px 20px;margin: 8px 0;box-sizing: border-box;border: none;border-radius: 4px;cursor: pointer;">完成本關卡</button>';
-echo '</form>';
 echo '
 <div id="scan_display"></div>
 <button id="scan" style="color:white;background-color: #E65100;width: 100%;padding: 12px 20px;margin: 8px 0;box-sizing: border-box;border: none;border-radius: 4px;cursor: pointer;">掃描QR Code</button>
 ';
+echo '<input type="text" id="pass" style="width: 100%;padding: 12px 20px;margin: 8px 0;box-sizing: border-box;" name="student" placeholder="請輸入通關者學號" />';
+echo '<button id="pass_button" style="color:white;background-color: #E65100;width: 100%;padding: 12px 20px;margin: 8px 0;box-sizing: border-box;border: none;border-radius: 4px;cursor: pointer;">完成本關卡</button>';
 echo '</div>';
 ?>

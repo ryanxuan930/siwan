8c081cffb89cb2e5aeeab42d16dab1fa34e1a744
ryanxuan930 <anakinchung930@gmail.com>
2020-11-19 14:02:49 +0800

    Update main.php

--- main/main.php
+++ main/main.php
@@ -2,9 +2,16 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Untitled Document</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="css/frame.css">
+<link rel="stylesheet" href="css/main.css">
+<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+<title>Seiawan</title>
 </head>
 <body>
-	<div>ok</div>
+	<div class="bg-img">
+		<div class="container_box">
+		</div>
+	</div>
 </body>
 </html>

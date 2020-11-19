6d99bbb326965b720109fa0f87d9dd0ee7e7734d
ryanxuan930 <anakinchung930@gmail.com>
2020-11-19 22:51:35 +0800

    Update pass.php

--- admin/module/point/pass.php
+++ admin/module/point/pass.php
@@ -11,26 +11,34 @@ while($row = $result->fetch_row()){
 switch($section){
 	case 1:
 		$sec = 'sec1';
+		$data = '舒壓植物園';
 		break;
 	case 2:
 		$sec = 'sec2';
+		$data = '打開新世界';
 		break;
 	case 3:
 		$sec = 'sec3';
+		$data = '趨勢聯想';
 		break;
 	case 4:
 		$sec = 'sec4';
+		$data = '溝通媒介';
 		break;
 	case 5:
 		$sec = 'sec5';
+		$data = '摩斯密碼';
 		break;
 	case 6:
 		$sec = 'sec6';
+		$data = '聽力輔助';
 		break;
 }
 $db->table('participant');
 $db->update($sec.'=1','stu_id="'.$stu_id.'"');
 if($db->error()){
 	echo $db->error();
+}else{
+	echo 
 }
 ?>

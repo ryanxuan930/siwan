2604df58f98fc2b5d651bde3d7bc43a5f5ea137f
ryanxuan930 <anakinchung930@gmail.com>
2020-11-19 22:27:08 +0800

    Update index.php

--- admin/module/point/index.php
+++ admin/module/point/index.php
@@ -37,7 +37,7 @@ function query(input){
 }
 $(document).ready(function(){
 	$("#pass_button").click(function(){
-		var input = $(#pass).val();
+		var input = $("#pass").val();
 		$.post("module/point/pass.php",{id: input, host: "'.$_SESSION['account'].'"},function(e){
 			alert(e);
 		});

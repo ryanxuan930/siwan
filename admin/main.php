0fa6e0fe998f21941969441993b4f4f78432d509
ryanxuan930 <anakinchung930@gmail.com>
2020-11-19 16:49:54 +0800

    Update main.php

--- admin/main.php
+++ admin/main.php
@@ -59,15 +59,15 @@ switch($permission){
 	<div id="container"></div>
 	<video id="preview"></video>
     <script>
-      let scanner = new Instascan.Scanner({ video: document.getElementById(\'preview\') });
-      scanner.addListener(\'scan\', function (content) {
+      let scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
+      scanner.addListener('scan', function (content) {
         console.log(content);
       });
       Instascan.Camera.getCameras().then(function (cameras) {
         if (cameras.length > 0) {
           scanner.start(cameras[0]);
         } else {
-          console.error(\'No cameras found.\');
+          console.error('No cameras found.');
         }
       }).catch(function (e) {
         console.error(e);

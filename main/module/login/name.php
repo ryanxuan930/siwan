d41d2e2181d9a99bb731b8ecf79804c7ba06c7d3
ryanxuan930 <anakinchung930@gmail.com>
2020-11-19 13:51:44 +0800

    Update name.php

--- main/module/login/name.php
+++ main/module/login/name.php
@@ -5,4 +5,6 @@ $name = $_POST['name'];
 $db = new database('seiawan');
 $db->table('participant');
 $db->update('name="'.$name.'"','stu_id="'.$_SESSION['id'].'"');
+header('Location: ../../main.php');
+exit();
 ?>

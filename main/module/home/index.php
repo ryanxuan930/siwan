759173ae49bdad67232f8dff4a40162454877788
ryanxuan930 <anakinchung930@gmail.com>
2020-11-20 23:51:41 +0800

    Update index.php

--- main/module/home/index.php
+++ main/module/home/index.php
@@ -70,7 +70,7 @@ if($present){
 	echo '<div class="border">你拿過獎品囉～ You have collected your reward.^^</div>';
 }
 if($data){
-	echo '<div class="border">西灣小天使：<hr>'.$data.'</div>';
+	echo '<div class="border">遊戲廣播：<hr>'.$data.'</div>';
 }
 echo '<script>';
 echo '$("#qrcode").qrcode({width: 256,height: 256,text: "'.$id.'"});';

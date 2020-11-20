864945bf004a06f02a6276fc4ce38bfe178c516e
ryanxuan930 <anakinchung930@gmail.com>
2020-11-20 23:57:46 +0800

    Update index.php

--- main/module/home/index.php
+++ main/module/home/index.php
@@ -72,6 +72,7 @@ if($present){
 if($data){
 	echo '<div class="border">遊戲廣播：<hr>'.$data.'</div>';
 }
+echo '<button>登出 Log Out</button>';
 echo '<script>';
 echo '$("#qrcode").qrcode({width: 256,height: 256,text: "'.$id.'"});';
 echo '</script>';

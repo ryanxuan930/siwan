6cf8228c7d037677975591f9aef2dde5634bdd03
ryanxuan930 <anakinchung930@gmail.com>
2020-11-21 00:19:58 +0800

    Create admin_server.php

---
+++ admin_server.php
@@ -0,0 +1,14 @@
+<?php
+header('Content-Type: text/event-stream');
+header('Cache-Control: no-cache');
+include('dbclass.php');
+$db = new database('seiawan');
+$db->table('data');
+$result = $db->select_all();
+while($row = $result->fetch_row()){
+	$id = $row[0];
+}
+echo "data: {$id}\n\n";
+flush();
+$db->update('refresh="0"','1=1');
+?>

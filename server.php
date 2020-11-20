2d0b7c35f4f6bb065ca3fda6ecb9858f4d07c9cc
ryanxuan930 <anakinchung930@gmail.com>
2020-11-20 17:27:10 +0800

    Update server.php

--- server.php
+++ server.php
@@ -1,9 +1,12 @@
 <?php
 header('Content-Type: text/event-stream');
 header('Cache-Control: no-cache');
-$id = 0;
-if(isset($_GET['id'])){
-	$id = $_GET['id'];
+include('dbclass.php');
+$db = new database('seiawan');
+$db->table('data');
+$result = $db->select_all();
+while($row = $result->fetch_row()){
+	$id = $row[0];
 }
 echo "data: {$id}\n\n";
 flush();

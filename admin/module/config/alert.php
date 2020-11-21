7138b24eb08d7acd7507c42a19c903b57f654043
ryanxuan930 <anakinchung930@gmail.com>
2020-11-21 14:49:54 +0800

    Update alert.php

--- admin/module/config/alert.php
+++ admin/module/config/alert.php
@@ -4,6 +4,7 @@ include('../../../dbclass.php');
 $content = $_POST['content'];
 $db = new database('seiawan');
 $db->table('data');
+$db->update('alert="'.$content.'"','1=1');
 echo '已發布通知';
 $db->update('refresh="alert", admin="alert"','1=1');
 ?>

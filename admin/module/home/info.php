b8d264124d62d522bf7723bf92bc8e81a5671f3b
ryanxuan930 <anakinchung930@gmail.com>
2020-11-21 13:25:01 +0800

    Update info.php

--- admin/module/home/info.php
+++ admin/module/home/info.php
@@ -1,10 +1,30 @@
 <?php
 include('../../../dbclass.php');
 $db = new database('seiawan');
-$db->table('data');
+$db->table('participant');
+$sec1 =0;
+$sec2 =0;
+$sec3 =0;
+$sec4 =0;
+$sec5 =0;
+$sec6 =0;
+$present =0;
 $result = $db->select_all();
 while($row = $result->fetch_row()){
-	echo $row[2];
+	$sec1 += $row[2];
+	$sec2 += $row[3];
+	$sec3 += $row[4];
+	$sec4 += $row[5];
+	$sec5 += $row[6];
+	$sec5 += $row[7];
+	$present += $row[9];
 }
-$db->update('admin=0','1=1');
+echo '<table>';
+echo '<tr><td>舒壓植物園</td><td>'.$sec1.'</td></tr>';
+echo '<tr><td>打開新世界</td><td>'.$sec2.'</td></tr>';
+echo '<tr><td>趨勢聯想</td><td>'.$sec3.'</td></tr>';
+echo '<tr><td>溝通媒介</td><td>'.$sec4.'</td></tr>';
+echo '<tr><td>摩斯密碼</td><td>'.$sec5.'</td></tr>';
+echo '<tr><td>聽力輔助</td><td>'.$sec6.'</td></tr>';
+echo '</table>';
 ?>

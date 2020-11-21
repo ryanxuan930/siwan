235b09aaf6a3ba39325f46071cb36ef0597d4345
ryanxuan930 <anakinchung930@gmail.com>
2020-11-21 16:09:16 +0800

    Update config.js

--- admin/js/config.js
+++ admin/js/config.js
@@ -5,7 +5,7 @@ function announceAdmin(){
 		configpage();
 	})
 }
-function announceFront(){
+function announceSubmit(){
 	var value = $("#front_announcement").val();
 	$.post("../admin/module/config/front_announce.php",{content: value},function(data){
 		alert(data);

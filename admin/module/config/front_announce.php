e7201346e601a48fa3263ad352868693f4451e77
ryanxuan930 <anakinchung930@gmail.com>
2020-11-21 15:58:50 +0800

    Update front_announce.php

--- admin/module/config/front_announce.php
+++ admin/module/config/front_announce.php
@@ -3,7 +3,6 @@ include('../../../dbclass.php');
 $content = $_POST['content'];
 $db = new database('seiawan');
 $db->table('data');
-$db->update('announce="'.$content.'"','1=1');
+$db->update('refresh="front", announce="'.$content.'"','1=1');
 echo '成功發布公告';
-$db->update('refresh="front"','1=1');
 ?>

d49f8525fd11bd3cf0a10dce41e4769a55c87a10
ryanxuan930 <anakinchung930@gmail.com>
2020-11-21 13:43:30 +0800

    Update index.php

--- admin/module/config/index.php
+++ admin/module/config/index.php
@@ -1,3 +1,7 @@
 <?php
-echo 'test';
+echo '<div class="console box">';
+echo '<h2>系統公告發布</h2><hr>';
+echo '<textarea id="announcement" style="min-height: 5ex; width:100%;"></textarea>';
+echo '<button class="btn" id="announce_submit">送出公告</button>';
+echo '</div>';
 ?>

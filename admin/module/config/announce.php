8fcb53e420d5bdbd3b75b6a70d9d9ba5fcf7acb3
ryanxuan930 <anakinchung930@gmail.com>
2020-11-21 14:15:44 +0800

    Update announce.php

--- admin/module/config/announce.php
+++ admin/module/config/announce.php
@@ -4,5 +4,5 @@ $content = $_POST['content'];
 $db = new database('seiawan');
 $db->table('data');
 $db->update('admin_ann="'.$content.'"','1=1')
-$db->update('admin=announce','1=1');
+$db->update('admin="announce"','1=1');
 ?>

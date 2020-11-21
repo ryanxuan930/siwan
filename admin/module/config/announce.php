6b7083b214cabcf33f0765feffa42cc378cbfbc3
ryanxuan930 <anakinchung930@gmail.com>
2020-11-21 14:12:44 +0800

    Create announce.php

---
+++ admin/module/config/announce.php
@@ -0,0 +1,8 @@
+<?php
+include('../../../dbclass.php');
+$content = $_POST['content'];
+$db = new database('seiawan');
+$db->table('data');
+$db->update('admin_ann="'.$content.'"','1=1')
+$db->update('admin=announce','1=1');
+?>

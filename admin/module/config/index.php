709a25772bf4b3dce244107a7565d958a94c84bb
ryanxuan930 <anakinchung930@gmail.com>
2020-11-21 16:09:18 +0800

    Update index.php

--- admin/module/config/index.php
+++ admin/module/config/index.php
@@ -8,7 +8,7 @@ echo '</div>';
 echo '<div class="console box">';
 echo '<h2>前台遊戲公告發布</h2><hr>';
 echo '<textarea id="front_announcement" style="height: 20ex; width:100%;margin: 10px 0; padding: 5px; border-radius: 4px;"></textarea>';
-echo '<button onClick="announceFront()" style="color:white;background-color: #E65100;width: 100%;padding: 12px 20px;margin: 8px 0;box-sizing: border-box;border: none;border-radius: 4px;cursor: pointer;">送出公告</button>';
+echo '<button onClick="announceSubmit()" style="color:white;background-color: #E65100;width: 100%;padding: 12px 20px;margin: 8px 0;box-sizing: border-box;border: none;border-radius: 4px;cursor: pointer;">送出公告</button>';
 echo '</div>';
 echo '<div class="console box">';
 echo '<h2>全域通知發布</h2><hr>';

7e2ddcfb8f1181de8a5f197d5b96ba6f68ce7b26
ryanxuan930 <anakinchung930@gmail.com>
2020-11-21 14:28:49 +0800

    Update config.js

--- admin/js/config.js
+++ admin/js/config.js
@@ -1,6 +1,6 @@
 function announceSubmit(){
 	var value = $("#announcement").val();
-	$.post("../admin/module/config/announce.php",{content: value},function(data){
+	$.post("../admin/module/config/admin_announce.php",{content: value},function(data){
 		alert(data);
 		configpage();
 	})

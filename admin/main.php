31cc21199308af56b6175c436dc94ba7c044f0c3
ryanxuan930 <anakinchung930@gmail.com>
2020-11-21 16:00:15 +0800

    Update main.php

--- admin/main.php
+++ admin/main.php
@@ -123,6 +123,7 @@ source.onmessage = function(event){
 	}
 	if(event.data == "announce"){
 		$.post("module/home/announce.php",function (data){
+			alert("有新公告");
 			$("#announce").html(data);
 		});
 	}

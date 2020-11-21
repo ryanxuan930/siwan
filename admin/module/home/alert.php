283e0d968928aff44c364878f529ac36d24c7795
ryanxuan930 <anakinchung930@gmail.com>
2020-11-21 14:46:27 +0800

    Update alert.php

--- admin/module/home/alert.php
+++ admin/module/home/alert.php
@@ -4,7 +4,8 @@ $db = new database('seiawan');
 $db->table('data');
 $result = $db->select_all();
 while($row = $result->fetch_row()){
-	echo $row[2];
+	echo $row[4];
 }
+sleep(5);
 $db->update('admin=0','1=1');
 ?>

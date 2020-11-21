c4243fc4d0148555ece3cea08201fd378649a6ae
ryanxuan930 <anakinchung930@gmail.com>
2020-11-21 13:52:48 +0800

    Update index.php

--- admin/module/config/index.php
+++ admin/module/config/index.php
@@ -1,7 +1,7 @@
 <?php
 echo '<div class="console box">';
 echo '<h2>系統公告發布</h2><hr>';
-echo '<textarea id="announcement" style="height: 20ex; width:100%;margin: 10px 0; padding: 5px; border-radius: 10px;"></textarea>';
+echo '<textarea id="announcement" style="height: 20ex; width:100%;margin: 10px 0; padding: 5px; border-radius: 4px;"></textarea>';
 echo '<button onClick="announceSubmit()" style="color:white;background-color: #E65100;width: 100%;padding: 12px 20px;margin: 8px 0;box-sizing: border-box;border: none;border-radius: 4px;cursor: pointer;">完成本關卡</button>';
 echo '</div>';
 ?>

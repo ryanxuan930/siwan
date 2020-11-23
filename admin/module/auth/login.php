f70d448ab07dc591753f2a6f24507d2c528296ca
ryanxuan930 <anakinchung930@gmail.com>
2020-11-23 16:10:12 +0800

    Update login.php

--- admin/module/auth/login.php
+++ admin/module/auth/login.php
@@ -19,7 +19,7 @@ if($result->num_rows){
 		header('Location: ../../main.php');
 		exit();
 	}else{
-		echo '<script>alert("密碼錯誤");location.href="../../index.php";</script>';
+		echo '<script>alert("密碼錯誤'.$password.' '.$hash.'");location.href="../../index.php";</script>';
 	}
 }else{
 	echo '<script>alert("帳號錯誤");location.href="../../index.php";</script>';

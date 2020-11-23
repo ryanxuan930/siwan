a3529f6ba7b0470c7dc759fbe965a21024244b25
ryanxuan930 <anakinchung930@gmail.com>
2020-11-23 21:36:43 +0800

    Update index.php

--- Main/index.php
+++ Main/index.php
@@ -19,20 +19,16 @@
 				<label for="id" style="color: lightgray">輸入你的學號，來開始未來之旅</label>
 			</div>
     		<input type="text" id="form_input" placeholder="Enter Your Student ID" name="id" maxlength="10">
-			<button type="submit" class="btn" onsubmit="return form()">GO</button>
+			<button type="submit" class="btn">GO</button>
+			<button type="button" class="btn" onClick="form()">生成序號 Generate</button>
 			<div>
-				<label style="color: #BDBDBD">If you don't have a student ID, don't worry. After you click the "GO" button, the system will automatically generate a unique serial number for you. BTW, DON'T FORGET IT, because it's your passcode in SIWAN~<br>如果你沒有學號也沒關係，按下GO以後，系統會自動幫你產出一組序號。喔還有！別忘了要記下這組序號，因為它是你接下來在西灣的通行碼唷～</label>
+				<label style="color: #BDBDBD">If you don't have a student ID, don't worry. After you press the "Generate" button, the system will automatically generate a unique serial number for you. BTW, DON'T FORGET IT, because it's your passcode in SIWAN~<br>如果你沒有學號也沒關係，按下「生成序號」以後，系統會自動幫你產出一組序號。喔還有！別忘了要記下這組序號，因為它是你接下來在西灣的通行碼唷～</label>
 			</div>
   		</form>
 	</div>
 <script>
 function form() {
-  	var x = document.forms["login_form"]["id"].value;
-	var code = Date.now().toString().substr(3,10);
-  	if(x == "" || x == null){
-    	document.getElementById("form_input").value = = code;
-    	return true;
-  	}
+  	document.getElementById("form_input").value = Date.now().toString().substr(3,10);
 }	
 </script>
 </body>

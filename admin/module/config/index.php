4f25ad5f0eb384a67cad67aecaaa7ab2db4c02f1
ryanxuan930 <anakinchung930@gmail.com>
2020-11-23 15:44:11 +0800

    Update index.php

--- admin/module/config/index.php
+++ admin/module/config/index.php
@@ -24,4 +24,8 @@ echo '<h2>全域通知發布</h2><hr>';
 echo '<textarea id="alert" style="height: 5ex; width:100%;margin: 10px 0; padding: 5px; border-radius: 4px;">'.$c3.'</textarea>';
 echo '<button onClick="alertSubmit()" style="color:white;background-color: #E65100;width: 100%;padding: 12px 20px;margin: 8px 0;box-sizing: border-box;border: none;border-radius: 4px;cursor: pointer;">送出通知</button>';
 echo '</div>';
+echo '<div class="console box">';
+echo '<h2>新增使用者</h2><hr>';
+echo '<button onClick="window.open(\'module/auth/account.php\')" style="color:white;background-color: #E65100;width: 100%;padding: 12px 20px;margin: 8px 0;box-sizing: border-box;border: none;border-radius: 4px;cursor: pointer;">送出公告</button>';
+echo '</div>';
 ?>

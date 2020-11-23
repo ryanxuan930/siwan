87225fb707172f84c7cfd055b50eb3cbdb7fb3f7
ryanxuan930 <anakinchung930@gmail.com>
2020-11-23 16:01:13 +0800

    Update login.php

--- admin/module/auth/login.php
+++ admin/module/auth/login.php
@@ -2,18 +2,27 @@
 include('../../../dbclass.php');
 include('../../../database.php');
 $account = $_POST['id'];
-$password = $_POST['password'];
+$hash = $_POST['password'];
+$hash = password_hash($hash, PASSWORD_DEFAULT);
 $conn->select_db('seiawan');
-$stmt = $conn->prepare("SELECT * FROM admin WHERE account=? AND password=?");
-$stmt->bind_param("ss", $account, $password);
+$stmt = $conn->prepare("SELECT * FROM admin WHERE account=?");
+$stmt->bind_param("s", $account);
 $stmt->execute();
 $result = $stmt->get_result();
 if($result->num_rows){
-	session_start();
-	$_SESSION['account'] = $account;
-	header('Location: ../../main.php');
-	exit();
+	while($row = $result->fetch_row()){
+		$password = $row[1];
+	}
+	if(password_verify($password, $hash)){
+		session_start();
+		$_SESSION['account'] = $account;
+		header('Location: ../../main.php');
+		exit();
+	}else{
+		echo '<script>alert("帳號或密碼錯誤");location.href="../../index.php";</script>';
+	}
 }else{
 	echo '<script>alert("帳號或密碼錯誤");location.href="../../index.php";</script>';
 }
+
 ?>

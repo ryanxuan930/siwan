2200e1391420cf000effe1456f478ac98f2e3cd5
ryanxuan930 <anakinchung930@gmail.com>
2020-11-23 16:02:21 +0800

    Update account.php

--- admin/module/auth/account.php
+++ admin/module/auth/account.php
@@ -6,12 +6,13 @@
 </head>
 <body>
 	<form action="encrypt.php" method="post">
-		<label for="account"></label>
+		<label for="account">帳號</label>
 		<input type="text" maxlength="10" name="account"/>
-		<label for="password"></label>
+		<label for="password">密碼</label>
 		<input type="password" name="password"/>
-		<label for="name"></label>
+		<label for="name">姓名</label>
 		<input type="text" name="name"/>
+		<label for="permission">權限</label>
 		<select name="permission">
 			<option value="1">企劃部</option>
 			<option value="2">行銷部</option>

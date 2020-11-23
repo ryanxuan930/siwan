9c8107d0171d287534685f9fb61c6561b15e7159
ryanxuan930 <anakinchung930@gmail.com>
2020-11-23 16:04:47 +0800

    Update encrypt.php

--- admin/module/auth/encrypt.php
+++ admin/module/auth/encrypt.php
@@ -9,8 +9,10 @@ $conn->select_db('seiawan');
 $stmt = $conn->prepare("INSERT INTO admin (account, password, name, permission, section) VALUES (?,?,?,?,1)");
 $stmt->bind_param("ssss", $account, $password, $name, $permission);
 $stmt->execute();
+/*
 echo '<script>
 	alert("已新增'.$account.' '.$name.'");
 	location.href="account.php";
 </script>';
+*/
 ?>

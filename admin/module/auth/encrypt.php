18cc32f4e9ac879d746bec19de98d5b542e1c698
ryanxuan930 <anakinchung930@gmail.com>
2020-11-23 15:52:34 +0800

    Update encrypt.php

--- admin/module/auth/encrypt.php
+++ admin/module/auth/encrypt.php
@@ -4,12 +4,13 @@ $account = $_POST['account'];
 $password = $_POST['password'];
 $name = $_POST['name'];
 $permission = $_POST['permission'];
+$password = password_hash($password, PASSWORD_DEFAULT);
 $conn->select_db('seiawan');
 $stmt = $conn->prepare("INSERT INTO admin (account, password, name, permission, section) VALUES (?,?,?,?,1)");
 $stmt->bind_param("ssss", $account, $password, $name, $permission);
 $stmt->execute();
 echo '<script>
-	alert("帳號或密碼錯誤");
+	alert("已新增'.$account.' '.$name.'");
 	location.href="account.php";
 </script>';
 ?>

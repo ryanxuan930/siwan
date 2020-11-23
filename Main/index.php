f466f0bd84b53425a5af0f9defa4391d3b5d5c53
ryanxuan930 <anakinchung930@gmail.com>
2020-11-23 21:32:04 +0800

    Update index.php

--- Main/index.php
+++ Main/index.php
@@ -18,7 +18,7 @@
 			<div>
 				<label for="id" style="color: lightgray">輸入你的學號，來開始未來之旅</label>
 			</div>
-    		<input type="text" placeholder="Enter Your Student ID" name="id" maxlength="10">
+    		<input type="text" id="form_input" placeholder="Enter Your Student ID" name="id" maxlength="10">
 			<button type="submit" class="btn" onsubmit="return form()">GO</button>
 			<div>
 				<label style="color: #BDBDBD">If you don't have a student ID, don't worry. After you click the "GO" button, the system will automatically generate a unique serial number for you. BTW, DON'T FORGET IT, because it's your passcode in SIWAN~<br>如果你沒有學號也沒關係，按下GO以後，系統會自動幫你產出一組序號。喔還有！別忘了要記下這組序號，因為它是你接下來在西灣的通行碼唷～</label>
@@ -30,7 +30,7 @@ function form() {
   	var x = document.forms["login_form"]["id"].value;
 	var code = Date.now().toString().substr(3,10);
   	if(x == "" || x == null){
-    	document.forms["login_form"]["id"].value = code;
+    	document.getElementById("form_input").value = = code;
     	return true;
   	}
 }	

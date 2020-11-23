3c054dbacb0a4e8cc0bf4c1c0221b1499444bd78
ryanxuan930 <anakinchung930@gmail.com>
2020-11-23 15:40:09 +0800

    Update encrypt.php

--- admin/module/auth/encrypt.php
+++ admin/module/auth/encrypt.php
@@ -1,13 +1,15 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>加密頁面</title>
-</head>
-<body>
-	<label for="account"></label>
-	<input type="text" maxlength="10" name="account"/>
-	<label for="password"></label>
-	<input type="password" name="password"/>
-</body>
-</html>
+<?php
+include('../../../database.php');
+$account = $_POST['account'];
+$password = $_POST['password'];
+$name = $_POST['name'];
+$permission = $_POST['permission'];
+$conn->select_db('seiawan');
+$stmt = $conn->prepare("INSERT INTO admin (account, password, name, permission, section) VALUES (?,?,?,?,1)");
+$stmt->bind_param("ssss", $account, $password, $name, $permission);
+$stmt->execute();
+echo '<script>
+	alert("帳號或密碼錯誤");
+	location.href="account.php";
+</script>';
+?>

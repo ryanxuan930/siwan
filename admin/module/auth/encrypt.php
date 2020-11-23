fa652bbea965a4d1a85a0a83701ef4152af95899
ryanxuan930 <anakinchung930@gmail.com>
2020-11-23 15:16:02 +0800

    Create encrypt.php

---
+++ admin/module/auth/encrypt.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>加密頁面</title>
+</head>
+<body>
+	<label for="account"></label>
+	<input type="text" maxlength="10" name="account"/>
+	<label for="password"></label>
+	<input type="password" name="password"/>
+</body>
+</html>
